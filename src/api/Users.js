3a7ba3d922efedb1b16dbe876bbab843005927b2
mithun <mithun@talenttakeaways.com>
2026-07-05 23:51:43 +0530

    Fix: Add missing users API file for Settings page

--- src/api/Users.js
+++ src/api/Users.js
@@ -1,12 +1,16 @@
 import api from "./axios";
 
 export const usersApi = {
-    getAll: () => api.get("/users").then(res => res.data),
-    getById: (id) => api.get(`/users/${id}`).then(res => res.data),
-    update:(id,data) => {
-        const cleanData = Object.fromEntries(
-            Object.entries(data).filter(([_,v]) => v !== "" && v !== null && v !== undefined)   
-        );
-        return api.patch(`/users/${id}`,cleanData).then(res => res.data);
-    }
-}
+  getAll: () => api.get("/users").then((res) => res.data),
+
+  getById: (id) => api.get(`/users/${id}`).then((res) => res.data),
+
+  update: (id, data) => {
+    const cleanData = Object.fromEntries(
+      Object.entries(data).filter(
+        ([_, v]) => v !== "" && v !== null && v !== undefined,
+      ),
+    );
+    return api.patch(`/users/${id}`, cleanData).then((res) => res.data);
+  },
+};
